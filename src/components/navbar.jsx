31a35ff6ca6da68235e236fd7a203b4b911f52ca
Wahyu NH <whyuunur@gmail.com>
2025-01-22 22:04:15 +0800

    feat/navbar and hero

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import "../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -15,7 +15,7 @@ function Navbar() {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.ScrollY > 50);
+      setIsScrolled(window.scrollY > 70);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -26,32 +26,57 @@ function Navbar() {
 
   return (
     <>
-      <nav className={`fixed w-full z-50 top-0 left-0 right-0 justify-between items-center flex shadow-md px-6 md:px-12 lg:px-14 xl:px-28 py-4 lg:py-5`}>
-        <Link to="/">
-          <img src="./logo.png" alt="logo-arifin" className="w-20 transition-all duration-300 hover:w-24" />
-        </Link>
-        <button className="text-2xl md:hidden transition-colors duration-300 hover:text-gray-500" onClick={handleToggle}>
+      <nav className={`fixed w-full z-50 top-0 left-0 right-0 justify-between items-center flex px-6 md:px-12 lg:px-14 xl:px-28 py-3 transition-all duration-300 ${isScrolled ? "bg-arifin-200 shadow-md" : "bg-transparent"} ${isOpen ? "bg-arifin-200 shadow-md" : ""}`}>
+        <button className={`text-2xl md:hidden transition-colors duration-300 ${isScrolled ? "text-arifin-500" : "text-arifin-200"}`} onClick={handleToggle}>
           <FontAwesomeIcon icon={faBars} />
         </button>
+        <Link to="/">
+          <img src={`${isScrolled ? "./logo.png" : "./logo 2.png"}`} alt="logo-arifin" className="w-11 lg:w-12  transition-all duration-300" />
+        </Link>
 
-        <div className="hidden md:flex gap-8 font-raleway font-medium md:text-md">
-          <Link to="/" className="transition-colors duration-300 hover:text-gray-500">
+        <div className={`hidden md:flex gap-8 font-raleway font-medium md:text-base  ${isScrolled ? "text-arifin-500" : "text-arifin-200"}`}>
+          <Link to="/" className="transition-colors duration-300 hover:text-arifin-400">
             Home
           </Link>
-          <Link to="/about" className="transition-colors duration-300 hover:text-gray-500">
+          <Link to="/about" className="transition-colors duration-300 hover:text-arifin-400">
             Produk
           </Link>
-          <Link to="/product" className="transition-colors duration-300 hover:text-gray-500">
+          <Link to="/product" className="transition-colors duration-300 hover:text-arifin-400">
             Feature
           </Link>
-          <Link to="/contact" className="transition-colors duration-300 hover:text-gray-500">
+          <Link to="/contact" className="transition-colors duration-300 hover:text-arifin-400">
             Tentang Kami
           </Link>
-          <Link to="/contact" className="transition-colors duration-300 hover:text-gray-500">
+          <Link to="/contact" className="transition-colors duration-300 hover:text-arifin-400">
             Kontak
           </Link>
         </div>
+
+        <div className={`fixed top-0 left-0 bottom-0 w-[65%] bg-arifin-100 z-40 transition-transform duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+          <div className="flex flex-col py-8 items-center h-full gap-4 text-lg">
+            <Link to="/" className="transition-colors duration-300 hover:text-arifin-400 w-full px-6">
+              Home
+            </Link>
+            <Link to="/about" className="transition-colors duration-300 hover:text-arifin-400 w-full px-6" >
+              Produk
+            </Link>
+            <Link to="/product" className="transition-colors duration-300 hover:text-arifin-400 w-full px-6">
+              Feature
+            </Link>
+            <Link to="/contact" className="transition-colors duration-300 hover:text-arifin-400 w-full px-6">
+              Tentang Kami
+            </Link>
+            <Link to="/contact" className="transition-colors duration-300 hover:text-arifin-400 w-full px-6">
+              Kontak
+            </Link>
+          </div>
+        </div>
+
+        <button className={`text-xl  transition-colors duration-300 ${isScrolled ? "text-arifin-500" : "text-arifin-200"}`} >
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
       </nav>
+      {isOpen && <div className="fixed inset-0 bg-black opacity-40 z-30 transition-all duration-300" onClick={handleToggle}></div>}
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "arifin-100": "#FFFDF8",
+        "arifin-200": "#FCF1DA",
+        "arifin-300": "#E8D6BE",
+        "arifin-400": "#D4A744",
+        "arifin-500": "#4A2C1A",
+      },
+    },
     fontFamily: {
       lora: ["Lora", "serif"],
       raleway: ["Raleway", "sans-serif"],
